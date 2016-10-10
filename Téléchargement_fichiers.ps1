8c2b436a8652dfb317d41daa2322b9ff872d61d8
MrFlynt <vialle.quentinalexandre@gmail.com>
2016-10-10 22:16:25 +0200

    Commented script

--- Téléchargement_fichiers.ps1
+++ Téléchargement_fichiers.ps1
@@ -1,5 +1,10 @@
-$url = "http://www.duchess-france.org/wp-content/uploads/2016/01/JeEzp.jpg"
-$output = "C:/Users/Q-A/Desktop/test.jpg"
-$start_time = Get-Date
+# Script to download file from powershell console
 
+# url of the file to download
+$url = "http://www.duchess-france.org/wp-content/uploads/2016/01/JeEzp.jpg"
+
+# path to save the file
+$output = "C:/Users/Q-A/Desktop/test.jpg"
+
+# Command to download and save the file
 Invoke-WebRequest -Uri $url -OutFile $output
